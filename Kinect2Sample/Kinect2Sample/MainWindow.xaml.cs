3e72da5ebcacbc934292669369dd73875c1f3dcd
w326004741 <326004741@qq.com>
2018-03-09 10:48:24 +0000

    Kinect Ex but doesnot work on vitural machine..so sad :(

---
+++ Kinect2Sample/Kinect2Sample/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kinect2Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private KinectSensor kinectSensor = null;
+        public MainWindow()
+        {
+            // one sensor is currently supported
+            this.kinectSensor = KinectSensor.GetDefault();
+            // open the sensor
+            this.kinectSensor.Open();
+            InitializeComponent();
+        }
+    }
+}
